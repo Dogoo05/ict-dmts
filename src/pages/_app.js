3b2ace325b6ed8b7c2fc630e9fa252584567e444
Dogoo05 <dogoo0740@gmail.com>
2025-09-30 15:15:55 +0800

    menu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
-import "@/styles/globals.css";
-import Footer from "../components/Footer";
-import Menu from "../components/Menu";
+import "../styles/globals.css";
+
 import { useRouter } from "next/router";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
