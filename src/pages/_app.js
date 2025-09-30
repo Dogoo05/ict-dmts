274d63fd15b280b83886ee9adc39cf20bbf74aff
Dogoo05 <dogoo0740@gmail.com>
2025-09-30 15:06:55 +0800

    appjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,19 @@
 import "@/styles/globals.css";
-import Footer from "./components/Footer";
-import Menu from "./components/Menu";
+import Footer from "../components/Footer";
+import Menu from "../components/Menu";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  const hideMenuFooterRoutes = ["/login"];
+  const shouldHideMenuFooter = hideMenuFooterRoutes.includes(router.pathname);
+
   return (
     <div>
-      <Menu />
+      {!shouldHideMenuFooter && <Menu />}
       <Component {...pageProps} />
-      <Footer />
+      {!shouldHideMenuFooter && <Footer />}
     </div>
   );
 }
